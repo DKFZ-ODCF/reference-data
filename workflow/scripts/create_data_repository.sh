e5a97202e46c216600d54c1d70e52b5094f15bc7
Fritjof Lammers <f.lammers@dkfz-heidelberg.de>
2020-08-14 14:08:18 +0200

    add verbosity

--- workflow/scripts/create_data_repository.sh
+++ workflow/scripts/create_data_repository.sh
@@ -4,15 +4,26 @@
 set -euf -o pipefail
 
 # Test if git and git annex are installed
-git --version  && echo "git installed"  || (echo "git not installed"; exit) &&
-git-annex version && echo "git-annex installed" || (echo "git annex not installed"; exit)
+git --version > /dev/null  && echo "git installed"  || (echo "git not installed"; exit) &&
+git-annex version > /dev/null && echo "git-annex installed" || (echo "git annex not installed"; exit)
 
 ASSEMBLYDIR_VARIANT=$1
 
-cd ${ASSEMBLYDIR_VARIANT} &&
 conda env create -f workflow/envs/git-annex.yml &&
 conda activate git-annex &&
+cd ${ASSEMBLYDIR_VARIANT} &&
 git init &&
 git annex init &&
 git annex add . &&
 git commit -m 'add files'
+
+echo "git annex repository created"
+echo ""
+echo "You may now clone this repository via "
+echo "$ git clone"
+echo "$ git annex init"
+echo "$ git remote add origin $(pwd) (TO BE CHANGED TO REMOTELY ACCESSIBLE)"
+echo "and selectively download files via "
+echo "% git annex sync origin"
+echo "$ git annex get <file>"
+echo "============================"
